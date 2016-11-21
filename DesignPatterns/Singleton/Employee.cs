cc624e9d0b8707f4f5c9e5e557a7b204b1ad1329
Jayakumar Mogenahalli <jayakumar.m.h@gmail.com>
2016-11-21 22:26:46 +0000

    final update

---
+++ DesignPatterns/Singleton/Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Employee
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FistName + " " + LastName;
+        }
+    }
+
+
+    /// <summary>
+    /// Not thread safe Singleton
+    /// </summary>
+    public class EmployeeService
+    {
+        private List<Employee> _employees;
+        private static EmployeeService _empService;
+
+        private EmployeeService()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public static EmployeeService EmployeeServiceInstance()
+        {
+            return _empService ?? (_empService = new EmployeeService());
+        }
+
+
+        public void AddEmployee(Employee emp)
+        {
+            _employees.Add(emp);
+        }
+
+        public List<Employee> GetEmployeeByLastName(string name)
+        {
+            return _employees.Where(e => e.LastName == name).ToList();
+        }
+
+    }
+
+    public class EmployeeServiceThreadSafe
+    {
+        private List<Employee> _employees;
+        private static EmployeeServiceThreadSafe _empService;
+
+        private static readonly object SyncRoot = new object();
+
+        private EmployeeServiceThreadSafe()
+        {
+            _employees = new List<Employee>();
+        }
+
+        /// <summary>
+        /// ThreadSafe Singleton
+        /// </summary>
+        /// <returns></returns>
+        public static EmployeeServiceThreadSafe EmployeeServiceInstance()
+        {
+            if (_empService == null) //First thread enter
+            {
+                lock (SyncRoot) // second thread see it is lock
+                {
+                    if (_empService == null) // second thread verify that instance created
+                    {
+                        _empService = new EmployeeServiceThreadSafe();
+                    }
+                }
+            }
+
+            return _empService;
+        }
+
+
+        public void AddEmployee(Employee emp)
+        {
+            _employees.Add(emp);
+        }
+
+        public List<Employee> GetEmployeeByLastName(string name)
+        {
+            return _employees.Where(e => e.LastName == name).ToList();
+        }
+    }
+}
+
+
--- DesignPatterns/Singleton/Program.cs
+++ DesignPatterns/Singleton/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Singleton;
 
 namespace ConsoleApplication1
 {
@@ -10,6 +11,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            //Static Class = inside static class only static property or class can be access, not non-static classes
+
+            //where as in Singleton you can instantiate non-static calss
+
+            EmployeeService employeeService = EmployeeService.EmployeeServiceInstance();
+            var emp1 = new Employee { FistName = "Jayakumar", LastName = "Mogenahalli" };
+            var emp2 = new Employee { FistName = "Savitha", LastName = "Jayakumar" };
+            var emp3 = new Employee { FistName = "Suman", LastName = "Jayakumar" };
+
+            employeeService.AddEmployee(emp1);
+            employeeService.AddEmployee(emp2);
+            employeeService.AddEmployee(emp3);
+
+            foreach (var e in employeeService.GetEmployeeByLastName("Jayakumar"))
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+
         }
     }
 }
